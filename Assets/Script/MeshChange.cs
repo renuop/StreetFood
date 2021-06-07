08a648bd5b44a25f995705f497b5fdec1f5133e6
arsstudio <arsstudio2021@gmail.com>
2021-06-07 13:20:07 +0530

    Changes

---
+++ Assets/Script/MeshChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshChange : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+
+    public Mesh meshBun;
+
+    public GameObject bun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+         
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+
+            meshFilter.mesh =meshBun;
+            transform.rotation = Quaternion.Euler(-90.0f, 0.0f, 160.0f);
+             Vector3 scaleChange =new Vector3(100f, 100f, 100f);
+           transform.localScale = scaleChange;
+        }
+    }
+}
